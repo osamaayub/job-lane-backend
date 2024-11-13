f8d4ad3daf9597c16d84c414c040a5f24eae110c
Osama Ayub <ayubosama416@gmail.com>
2024-11-14 01:49:03 +0500

    Deleted validation middlewares

--- routes/ApplicationRoutes.js
+++ routes/ApplicationRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const {isAuthenticated} = require('../middlewares/auth')
 const {createApplication, getSingleApplication, getUsersAllApplications, deleteApplication} = require('../controllers/ApplicationControllers')
-const {applicationIdValidator,validateHandler} = require('../middlewares/validators');
 const router = express.Router()
 
 
-router.route('/createApplication/:id').post(isAuthenticated,applicationIdValidator(),validateHandler, createApplication)
+router.route('/createApplication/:id').post(isAuthenticated, createApplication)
 
-router.route('/singleApplication/:id').get(isAuthenticated,applicationIdValidator(),validateHandler, getSingleApplication)
+router.route('/singleApplication/:id').get(isAuthenticated, getSingleApplication)
 
 router.route('/getAllApplication').get(isAuthenticated, getUsersAllApplications)
 
-router.route('/deleteApplication/:id').delete(isAuthenticated,applicationIdValidator(),validateHandler, deleteApplication)
+router.route('/deleteApplication/:id').delete(isAuthenticated, deleteApplication)
 
 
 module.exports = router 
