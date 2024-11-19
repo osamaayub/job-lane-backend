d4d203105190cf4d3c72a186b6253b7416b16a8b
Osama Ayub <ayubosama416@gmail.com>
2024-11-19 14:17:35 +0500

    added multer for file handling the logo

--- routes/JobRoutes.js
+++ routes/JobRoutes.js
@@ -1,16 +1,25 @@
 const express = require('express')
+const upload = require('../utils/multer');
 const { isAuthenticated, authorizationRoles } = require('../middlewares/auth')
-const {createJob, allJobs, oneJob, saveJob, getSavedJobs} = require('../controllers/JobControllers')
+const {
+  createJob,
+   allJobs, 
+   oneJob, 
+   saveJob, 
+   getSavedJobs
+  } = require('../controllers/JobControllers');
 const router = express.Router()
 
 
-router.route("/create/job").post(isAuthenticated, authorizationRoles("admin") , createJob)
+router.route("/create/job")
+.post(isAuthenticated, authorizationRoles("admin"),
+upload.single('companyLogo'),  createJob)
 
 router.route("/jobs").get(allJobs) ;
 
 router.route("/job/:id").get(oneJob) ;
 
-router.route("/saveJob/:id").get(isAuthenticated, saveJob) ;
+router.route("/saveJob/:id").get(isAuthenticated,saveJob) ;
 
 router.route("/getSavedJobs").get(isAuthenticated, getSavedJobs) ;
 
