f4d305fd636be833257e93bfb09abe4cfa527f13
Osama Ayub <ayubosama416@gmail.com>
2024-11-19 14:22:08 +0500

    fixed the extra validation issue

--- models/AppModel.js
+++ models/AppModel.js
@@ -4,12 +4,10 @@ const ApplicationSchema =  new mongoose.Schema({
     job: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Job',
-        required: true
     },
     applicant: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
     },
     applicantResume: {
         public_id: {
