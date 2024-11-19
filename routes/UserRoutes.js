004993d75af936f6eb2122629aaa3e92671edfb7
Osama Ayub <ayubosama416@gmail.com>
2024-11-19 14:16:44 +0500

    added multer for file handling in user Route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,16 +1,44 @@
-const express = require('express')
-const { register, login, isLogin, me, changePassword, updateProfile, deleteAccount } = require('../controllers/UserControllers')
+const express = require('express');
+const {
+  register,
+  login,
+  isLogin,
+  me,
+  changePassword,
+  updateProfile,
+  deleteAccount 
+} = require('../controllers/UserControllers')
+const upload = require("../utils/multer");
 const { isAuthenticated } = require('../middlewares/auth');
 const router = express.Router()
 
+router
+  .route("/register")
+  .post(
+    upload.fields([
+      { name: "avatar", maxCount: 1 },
+      { name: "resume", maxCount: 1 },
+    ]),
+    register
+  );
 
-router.route("/register").post( register);
-router.route("/login").post( login);
+
+
+router.route("/login").post(login);
 router.route("/isLogin").get(isAuthenticated, isLogin);
 router.route("/me").get(isAuthenticated, me);
-router.route("/changePassword").put(isAuthenticated,  changePassword);
-router.route("/updateProfile").put(isAuthenticated, updateProfile);
-router.route("/deleteAccount").put(isAuthenticated,   deleteAccount);
+router.route("/changePassword").put(isAuthenticated, changePassword);
+router.route("/updateProfile")
+  .put(
+    isAuthenticated,
+    upload.fields([
+      { name: "newAvatar", maxCount: 1 },
+      { name: "newResume", maxCount: 1 }
+    ]),
+    updateProfile
+  );
+
+router.route("/deleteAccount").delete(isAuthenticated, deleteAccount);
 
 
 
